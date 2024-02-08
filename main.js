be2aa5bde51a5b147d7f6f34b6862987d41ac8c8
Jorge_Barrios <jorge.barriosn@usm.cl>
2024-02-08 10:35:19 -0300

    feat: React proyect

--- main.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-const url = 'https://api.boostr.cl/vehicle/CPRL32.json';
-const options = {method: 'GET', headers: {accept: 'application/json'}};
-
-fetch(url, options)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.error('error:' + err));
